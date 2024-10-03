ee9f170e8364c7ee3b23e32ab516cc05f48b4107
KyleRaven <Kyleravendelarama01@outlook.com>
2024-10-03 10:25:25 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	string name;
+	int age;
+	
+	cout << " Our Lady Of Lourdes College Foundation " << endl;
+	cout << " College of Computer Studies" << endl;
+	cout << " BSIT 1-B" << endl;
+	cout << " Kyle Raven A. Dela Rama" << endl;
+	cout << " Purok 4 Bagasbas Daet, Camarines Norte" << endl;
+	return 0;
+	
+	}
+	
