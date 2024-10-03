79fb826f09a23fc497d218f53df0ccc2397d8b00
KyleRaven <Kyleravendelarama01@outlook.com>
2024-10-03 10:44:29 +0800

    Add files via upload

---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+	int age;
+	
+	cout << "Enter your age \t|\t";
+	cin >> age;
+	cout << endl;
+	cout << "Your Age is " << age << " Thank You and Have a Great Day!" << endl;
+	return 0;
+}
